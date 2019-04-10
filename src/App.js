e8937a42052141f8ea72269d83f0ae398a1765ea
Preston Carey <pcarey1213@gmail.com>
2019-04-10 18:31:48 -0400

    presentable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }from 'react';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Container from './components/Container';
+import Clicky from './components/Clicky';
+import composers from "./composers.json";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    composers
+  };
+
+  shuffleComposers = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const composers = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ composers });
+
+
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar />
+        <Header />
+        <Container>
+        {this.state.composers.map(composer => (
+          <Clicky
+            id={composer.id}
+            name={composer.name}
+            image={composer.image}
+          />
+        ))}
+        </Container>
       </div>
     );
   }
