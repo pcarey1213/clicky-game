cc8a9c9980c4994d5dee809014960828bb30f02a
Preston Carey <pcarey1213@gmail.com>
2019-04-11 00:57:04 -0400

    Updates

---
+++ precache-manifest.e449d8af65dd9858892ca17fb8601822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "b7499ee43b0f217632c7",
+    "url": "/clicky-game/static/js/main.23e6f28c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b7499ee43b0f217632c7",
+    "url": "/clicky-game/static/css/main.90c908c5.chunk.css"
+  },
+  {
+    "revision": "bea2c705a2a74d367c1a709ea66601fe",
+    "url": "/clicky-game/index.html"
+  }
+];
--- src/components/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Container(props) {
-  return (
-    <main class="container">{props.children}</main>
-  );
-}
-
-export default Container;
